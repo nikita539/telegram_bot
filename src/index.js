db7b888d9e8fce766680d325ea55323663279c13
nikita539 <evteev_n@inbox.ru>
2023-05-31 13:16:19 +0300

    created command to register  words

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const Telegraf = require('telegraf')
 require('dotenv').config();
-const words = require('./words.json')
+const words = require('./words.json');
+const { getLength, wordIs } = require('./utils');
 
 
 const bot = new Telegraf.Telegraf(process.env.BOT_TOKEN)
@@ -25,17 +26,28 @@ bot.start(function(context) {
 })
 
 bot.command('register_words', function(context) {
-    
+    bot.on('text', function(context) {
+        const splited_word = context.message.text.split('-')
+
+        if (wordIs(splited_word[0], words)) {
+            context.sendMessage('такое слово уже есть')
+            return
+        }
+
+        const word = {
+            word: splited_word[0],
+            translate: splited_word[1],
+            count_showed: 0,
+            right_answered: 0
+        }
+        words[getLength(words)] = word
+        console.log(words)
+    })
 })
 
 bot.command('words', function(context) {
-    const learning_words = []
-
-    Object.keys(words).forEach((key) => {
-        learning_words.push(`${words[key].word} - ${words[key].translate}`)
-    })
-
-    context.sendMessage(learning_words.join('\n'))
+    const result = Object.keys(words).map((key) => `${words[key].word} - ${words[key].translate}`)
+    context.sendMessage(result.join('\n'))
 })
 
 bot.command('learn', function(context) {
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,11 @@ function getLength(obj) {
     return Array.isArray(obj) ? obj.length : Object.keys(obj).length
 }
 
+function wordIs(word, obj) {
+    return  Object.keys(obj).some((key) => obj[key].word === word)
+}
+
 module.exports = {
-    getLength
+    getLength,
+    wordIs
 }
