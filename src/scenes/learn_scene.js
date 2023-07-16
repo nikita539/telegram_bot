902e9901d78fbf59521beaa25e1097cc51ae39e9
nikita539 <evteev_n@inbox.ru>
2023-07-17 00:48:13 +0300

    small refactoring. Crated function for sending word

--- src/scenes/learn_scene.js
+++ src/scenes/learn_scene.js
@@ -11,7 +11,7 @@ const {
 const learn_scene = new Scenes.BaseScene(SCENES_NAMES.learn_words)
 
 // TO-DO: алгоритм работы сцены. Сцена присылает слово на аглийском, в ответ на это сообщение
-// пользователь присылает перевод этого слова, если перевод верный, сцена присылает новое слово,
+// пользователь присылает перевод этого слова, если перевод верный, сцена присылает новое слово
 
 
 learn_scene.enter(async (context) => {
@@ -25,8 +25,8 @@ learn_scene.leave((context) => {
 })
 
 learn_scene.on('text', async function(context) {
-    function sendWord() {
-        context.reply(`${words[context.scene.state.counter]?.word}`)
+    async function sendWord() {
+        await context.reply(`${words[context.scene.state.counter]?.word}`)
         context.scene.state.sent_word = words[context.scene.state.counter]
         context.scene.state.counter++
     }
@@ -34,24 +34,18 @@ learn_scene.on('text', async function(context) {
     if (context.scene.state.sent_word.translate === context.message.text.toLocaleLowerCase()) {
         if ( context.scene.state.counter === getLength(words) ) {
             context.scene.state.counter = 0
-            context.scene.state.sent_word = words[context.scene.state.counter]
-            await context.reply(` ${words[context.scene.state.counter]?.word}`)
-            context.scene.state.counter++
+            sendWord()
             return
         }
 
         words[context.scene.state.counter-1].right_answered++
         words[context.scene.state.counter-1].count_showed++
-        context.reply(`${words[context.scene.state.counter]?.word}`)
-        context.scene.state.sent_word = words[context.scene.state.counter]
-        context.scene.state.counter++
+        sendWord()
         return
     } else {
         if (context.scene.state.mistake == 4) {
             context.reply(`это слово преаводится так ${words[context.scene.state.counter].translate}, запомни) `)
-            context.scene.state.counter++
-            context.scene.state.sent_word = words[context.scene.state.counter]
-            context.reply(`${words[context.scene.state.counter]?.word}`)
+            sendWord()
             context.scene.state.mistake = 0
             return
         }
